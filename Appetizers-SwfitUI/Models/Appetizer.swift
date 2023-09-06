85b6eef52270f1851bad34838a8fb2acd6e89c9b
Manuel Alejandro <alejandro>
2023-09-06 10:33:45 -0700

    Creando modelo para appetizer

---
+++ Appetizers-SwfitUI/Models/Appetizer.swift
@@ -0,0 +1,19 @@
+//
+//  Appetizer.swift
+//  Appetizers-SwfitUI
+//
+//  Created by Administrador on 06/09/23.
+//
+
+import Foundation
+
+struct Appetizer {
+    let id: Int
+    let imageURL: String
+    let calories: Int
+    let name: String
+    let protein: Int
+    let description: String
+    let price: Double
+    let carbs: Int
+}
