b87960e70c6c12ca20b6483e2ba8bb46e197d337
Manuel Alejandro <alejandro>
2023-09-18 17:02:43 -0700

    Creando form y secciones

--- Appetizers-SwfitUI/Screens/AccountView.swift
+++ Appetizers-SwfitUI/Screens/AccountView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName: String = String()
+    @State private var lastName: String = String()
+    @State private var email: String = String()
+    @State private var birthDay: Date = .now
+    @State private var extraNapkins = false
+    @State private var frequentRefils = false
+    
     var body: some View {
         NavigationView {
-            Text("Account").navigationTitle("😀 Account")
+            Form {
+                Section {
+                    TextField("First name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                    TextField("email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                    DatePicker("Birthday", selection: $birthDay, displayedComponents: .date)
+                    Button {
+                        
+                    } label: {
+                        Text("Save changes")
+                    }
+                } header: {
+                    Text("Personal info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefils)
+                } header: {
+                    Text("Requests")
+                }.toggleStyle(SwitchToggleStyle(tint: Colors.brandPrimary))
+
+
+            }.navigationTitle("😀 Account")
         }
     }
 }
